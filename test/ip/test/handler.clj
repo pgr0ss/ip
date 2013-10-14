d7ee4e70084fa134d904197be9ea980c30c4c8d9
Paul Gross <pgross@gmail.com>
2013-10-14 15:37:36 -0700

    Create a new leiningen project.

---
+++ test/ip/test/handler.clj
@@ -0,0 +1,14 @@
+(ns ip.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        ip.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
