937a418eb8878d76ad56292b9744a71e3152d674
Paul Gross <pgross@gmail.com>
2013-10-14 16:20:33 -0700

    Get app running on heroku.

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
   :plugins [[lein-ring "0.8.5"]]
+  :min-lein-version "2.0.0"
   :ring {:handler ip.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src/ip/handler.clj
+++ src/ip/handler.clj
@@ -2,7 +2,8 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [ring.adapter.jetty :as jetty]))
 
 (defn- format-header [[header-name value]]
   (format "%s: %s" (string/capitalize header-name) value))
@@ -21,3 +22,11 @@
 
 (def app
   (handler/site app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port 3000))]
+    (jetty/run-jetty app {:port port :join? false})))
+
+(defn -main []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (jetty/run-jetty app {:port port :join? false})))
